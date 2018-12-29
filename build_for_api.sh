2542ba9cbfceb69cef6352b707148763b6bf3505
Loïc Lopez <loic.lopez@epitech.eu>
2018-12-28 23:31:04 -0500

    fix for laravel-artisan

--- build_for_api.sh
+++ build_for_api.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
+mkdir laravel-artisan/horizon
+mkdir laravel-artisan/daemon
+
 cp -R ../exosuite-users-api php-fpm
 cp -R ../exosuite-users-api nginx
-cp -R ../exosuite-users-api laravel-artisan
+cp -R ../exosuite-users-api laravel-artisan/horizon
+cp -R ../exosuite-users-api laravel-artisan/daemon
 
 python3.5 nginx/build.py --api
 python3.5 php-fpm/build.py --api
-
-mkdir laravel-artisan/horizon
-mkdir laravel-artisan/daemon
 
 python3.5 laravel-artisan/build.py --horizon
 mv laravel-artisan/Dockerfile laravel-artisan/horizon
