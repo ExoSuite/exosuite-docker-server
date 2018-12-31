ab54e461e2176e4b44d1254bdc28c3b08e269ea3
Loïc Lopez <loic.lopez@epitech.eu>
2018-12-31 02:39:21 -0500

    fix fox website

--- build_for_website.sh
+++ build_for_website.sh
@@ -2,7 +2,6 @@
 
 cp -R ../exosuite-website php-fpm
 cp -R ../exosuite-website nginx
-cp -R ../exosuite-website laravel-artisan
 
-python3.5 nginx/build.py --api
-python3.5 php-fpm/build.py --api
+python3.5 nginx/build.py --website
+python3.5 php-fpm/build.py --website
