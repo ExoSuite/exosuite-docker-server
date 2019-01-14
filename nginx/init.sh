308d9aa6fb60126a4ffeecdaeeb12856afa95d5d
Loïc Lopez <loic.lopez@epitech.eu>
2019-01-14 16:18:59 -0500

    try fix init

--- nginx/init.sh
+++ nginx/init.sh
@@ -2,7 +2,4 @@
 
 chown -R nginx:nginx ${APP_DIR}
 
-find  ${APP_DIR} -type f -exec chmod 644 {} \;
-find  ${APP_DIR} -type d -exec chmod 755 {} \;
-
 chmod -R ug+rwx ${APP_DIR}/storage ${APP_DIR}/bootstrap/cache
--- php-fpm/init.sh
+++ php-fpm/init.sh
@@ -5,16 +5,13 @@ do
     php ${APP_DIR}/artisan ${command}
 done
 
-chown -R www-data:www-data ${APP_DIR}
-
-find  ${APP_DIR} -type f -exec chmod 644 {} \;
-find  ${APP_DIR} -type d -exec chmod 755 {} \;
-
 rm bootstrap/cache/*.php
 php ${APP_DIR}/artisan optimize:clear
 php ${APP_DIR}/artisan package:discover
 php ${APP_DIR}/artisan config:cache
 php ${APP_DIR}/artisan route:cache
 php ${APP_DIR}/artisan view:cache
+
+chown -R www-data:www-data ${APP_DIR}
 
 chmod -R ug+rwx ${APP_DIR}/storage ${APP_DIR}/bootstrap/cache
