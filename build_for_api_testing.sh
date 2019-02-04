f026c917c5442d58f3b77e96bb80d7891517d131
Loïc Lopez <loic.lopez@epitech.eu>
2019-02-04 16:40:02 -0500

    add memcached

--- build_for_api_testing.sh
+++ build_for_api_testing.sh
@@ -1,14 +1,5 @@
 #!/usr/bin/env bash
 
-mkdir laravel-artisan/horizon
-mkdir laravel-artisan/daemon
-
-./copy_files_for_artisan.sh
-
 python3.5 php-fpm/build.py --api --testing
 
 python3.5 laravel-artisan/build.py --horizon --testing
-mv laravel-artisan/Dockerfile laravel-artisan/horizon
-
-python3.5 laravel-artisan/build.py --daemon --testing
-mv laravel-artisan/Dockerfile laravel-artisan/daemon
