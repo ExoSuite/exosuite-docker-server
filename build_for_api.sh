c05246efbc89d56d676921277b22ace37b69d85c
Loïc Lopez <loic.lopez@epitech.eu>
2019-03-17 11:26:20 -0400

    add scheduler directory to script

--- build_for_api.sh
+++ build_for_api.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-mkdir laravel-artisan/horizon
-mkdir laravel-artisan/daemon
+mkdir -p laravel-artisan/horizon
+mkdir -p laravel-artisan/scheduler
 
 cp -R ../exosuite-users-api php-fpm
 cp -R ../exosuite-users-api nginx
 cp -R ../exosuite-users-api laravel-artisan/horizon
-cp -R ../exosuite-users-api laravel-artisan/daemon
+cp -R ../exosuite-users-api laravel-artisan/scheduler
 
 ./copy_files_for_artisan.sh
 
@@ -17,4 +17,4 @@ python3.5 laravel-artisan/build.py --horizon
 mv laravel-artisan/Dockerfile laravel-artisan/horizon
 
 python3.5 laravel-artisan/build.py --daemon
-mv laravel-artisan/Dockerfile laravel-artisan/daemon
+mv laravel-artisan/Dockerfile laravel-artisan/scheduler
--- copy_files_for_artisan.sh
+++ copy_files_for_artisan.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 cp laravel-artisan/init.sh laravel-artisan/horizon
-cp laravel-artisan/init.sh laravel-artisan/daemon
+cp laravel-artisan/init.sh laravel-artisan/scheduler
 cp laravel-artisan/php.ini laravel-artisan/horizon
-cp laravel-artisan/php.ini laravel-artisan/daemon
+cp laravel-artisan/php.ini laravel-artisan/scheduler
 
